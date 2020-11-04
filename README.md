40d3ff9ab0d171c83a3f65445c286a12d30a059a
Max Brockbank <maxbrockbank1999@gmail.com>
2020-11-03 16:33:57 -0800

    add user interface logic
    co-authored-by: Svea Wade <svealinneawade@gmail.com>

--- README.md
+++ README.md
@@ -7,23 +7,37 @@
 <p align="center">Updated on Nov 1, 2020</p>
 
 ## Description
-
+Bank account application that allows you to start new accounts and see the account details.
 
 ## Set-up Instructions
-1. 
+1. Clone this repo to your desktop.
+2. Open index.html in the browser of your choice.
+* _Alternatively you can just click [HERE](#)_
 
 ## Technologies Used
-* 
+* HTML
+* CSS/Bootstrap
+* JavaScript/jQuery
 
 ## Known Bugs
+* 
 
 ## Specs
-* Describe: bankAccount()
+### Describe: bankAccount()
 
-* Test: create new account object
-* Expect:
+ 1. Test: Should correctly create new bank object
+  * Expect(bankAccount().toEqual(newBank))
+
+2. Test: Should correctly create new account object
+* Expect(bankAccount().toEqual(newAccount))
+
+3. Test: Should correctly add new account object to bank object
+* Expect(bankAccount(newAccount).toEqual(newBank.newAccount))
+
+4. Test: Should correctly loop through bank object accounts and display on the HTML page
+*  Expect(bankAccount(newBank.newAccount).toEqual(pageDisplay.html(newBank.newAccount)))
 
 
 ## Legal
-* Copyright © 2020 Max Brockbank
+* Copyright © 2020 Max Brockbank & Svea Wade
 * This software is licensed under the MIT license
--- js/scripts.js
+++ js/scripts.js
@@ -10,14 +10,18 @@ Bank.prototype.addAccount = function(account){
   this.accounts.push(account);
 };
 
-
-
-
+Bank.prototype.getAccount = function(id){
+  for(let i = 0; this.accounts.length; i++){
+    if(this.accounts[i].id == id){
+      return this.accounts[i];
+    }
+  }
+}
 //Account Business Logic
-function Account(name, address, deposit){
+function Account(name, address, amount){
   this.name = name;
   this.address = address;
-  this.deposit = deposit;
+  this.amount = amount;
 };
 
 
@@ -29,29 +33,53 @@ function showAccountInfo(accountsToDisplay) {
   let accountInfo = $("div#showAccount");
   let htmlForNewAccount = "";
   accountsToDisplay.forEach(function(account) {
-    htmlForNewAccount += "<p> Name: " + account.name +"</p> <p> Address: " + account.address +"</p> <p> Deposit: " + account.deposit +"</p>"
+    htmlForNewAccount += "<div class ='account'><p> Name: " + account.name +"</p> <p> Address: " + account.address +"</p> <p> Amount: $" + account.amount +"</p><button class='newTransaction btn btn-dark'id="+account.id+">Transaction</button></div>"; 
     
   });
-  accountInfo.html(htmlForNewAccount);
-  accountInfo.show();
-  //$("#show").show();
-  //$("#name").html(account.name);
-  //$("#address").html(account.address);
-  //$("deposit").html(account.deposit);
+  accountInfo.html(htmlForNewAccount); 
+  $("#accountShow").show();
 };
+
+function enableTransactions(bank){
+  let currentAccount;
+  $(".newTransaction").on('click', function(){
+    currentAccount = bank.getAccount(this.id);
+    $("#current-account").text(currentAccount.name + ": $" + currentAccount.amount);
+    $("#transactions").show();
+  })
+  $("#deposit").on('click', function(){
+    let deposit = parseInt($("#transactionAmount").val());
+    console.log(currentAccount);
+    let balance = parseInt(currentAccount.amount);
+    let total = balance + deposit;
+    currentAccount.amount = total;
+    console.log(currentAccount.amount)
+    $("#transactions").hide();
+    showAccountInfo(bank.accounts)
+  })
+  $("#withdraw").on('click', function() {
+    let withdraw = parseInt($("#transactionAmount").val());
+    let total = balance - withdraw;
+    currentAccount.amount = total;
+    console.log(currentAccount.amount)
+    $("#transactions").hide();
+    showAccountInfo(bank.accounts)
+  })
+}
+
 
 $(document).ready(function(){
   let firstBank = new Bank();
-
   $("#new-account").submit(function(event){
     event.preventDefault();
     const name = $("#name").val();
     const address = $("#address").val();
-    const deposit = $("#deposit").val();
+    const amount = $("#amount").val();
 
-    let newAccount = new Account(name, address, deposit);
+    let newAccount = new Account(name, address, amount);
     firstBank.addAccount(newAccount);
     showAccountInfo(firstBank.accounts);
     console.log(firstBank);
+    enableTransactions(firstBank);
   });
 });
